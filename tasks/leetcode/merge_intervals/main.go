d2677ecad6e6fe4ff9180f02c081c2e81a967896
SAENKO Dmitry <dmitryssaenko@gmail.com>
2021-02-01 21:03:03 +0300

    add merge_intervals solution

--- tasks/leetcode/merge_intervals/main.go
+++ tasks/leetcode/merge_intervals/main.go
@@ -5,37 +5,35 @@ import (
 	"sort"
 )
 
-//type intervalsType [][]int
-//
-//func (a intervalsType) Len() int {
-//	return len([][]int(a))
-//}
-//
-//func (a intervalsType) Swap(i, j int) {
-//	a[i], a[j] = a[j], a[i]
-//}
-//
-//func (a intervalsType) Less(i, j []int) bool {
-//	if i[0] == j[0] {
-//		return i[0] < j[0]
-//	}
-//	return i[1] < j[1]
-//}
+type intervalsType [][]int
+
+func (a intervalsType) Len() int {
+	return len([][]int(a))
+}
+
+func (a intervalsType) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a intervalsType) Less(i, j int) bool {
+	if a[i][0] == a[j][0] {
+		return a[i][0] < a[j][0]
+	}
+	return a[i][0] < a[j][0]
+}
 
 func merge(intervals [][]int) [][]int {
-
 	result := make([][]int, 0)
 
+	//sort.Sort(intervalsType(intervals))
 	sort.Slice(intervals, func(i, j int) bool {
 		if intervals[i][0] == intervals[j][0] {
 			return intervals[i][0] < intervals[j][0]
 		}
-		return intervals[i][1] < intervals[j][1]
+		return intervals[i][0] < intervals[j][0]
 	})
 
-	fmt.Println(intervals)
-	for i := range intervals {
-
+	for i := 0; i < len(intervals); i++ {
 		currentLeft, currentRight := intervals[i][0], intervals[i][1]
 		for j := i + 1; j < len(intervals); j++ {
 
@@ -44,10 +42,41 @@ func merge(intervals [][]int) [][]int {
 				if next[1] > currentRight {
 					currentRight = next[1]
 				}
-				intervals = append(intervals[:j + 1], intervals[j + 1:]...)
+				intervals = append(intervals[:j], intervals[j + 1:]...)
 				j--
 			} else {
-				continue
+				break
+			}
+		}
+		result = append(result, []int{currentLeft, currentRight})
+	}
+
+	return result
+}
+
+func merge2(intervals [][]int) [][]int {
+	result := make([][]int, 0)
+
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] == intervals[j][0] {
+			return intervals[i][0] < intervals[j][0]
+		}
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	for i := 0; i < len(intervals); i++ {
+		currentLeft, currentRight := intervals[i][0], intervals[i][1]
+		for j := i + 1; j < len(intervals); j++ {
+
+			next := intervals[j]
+			if next[0] >= currentLeft && next[0] <= currentRight {
+				if next[1] > currentRight {
+					currentRight = next[1]
+				}
+				intervals = append(intervals[:j], intervals[j + 1:]...)
+				j--
+			} else {
+				break
 			}
 		}
 		result = append(result, []int{currentLeft, currentRight})
@@ -58,5 +87,8 @@ func merge(intervals [][]int) [][]int {
 
 func main() {
 	intervals := [][]int{{2,6},{1,3},{8,10},{15,18}}
+	//intervals := [][]int{{1,4},{4,5}}
+	//intervals := [][]int{{1,4},{0,4}}
+	//intervals := [][]int{{1,4},{2,3}}
 	fmt.Println(merge(intervals))
 }
