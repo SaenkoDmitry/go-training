a43f9f23752d6a4db885d49d42dd47695eaf33d5
DMITRY SAENKO <dmitry.saenko@alibaba-inc.com>
2021-06-15 01:37:50 +0300

    race condition problem solve

---
+++ tasks/race_condition/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var counter int64
+	var wg sync.WaitGroup
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup) {
+			atomic.AddInt64(&counter, 1)
+			wg.Done()
+		}(&wg)
+	}
+	wg.Wait()
+	fmt.Println(counter)
+}
