141bfe35a4f7b7e5895e7c100c055a423ea15ab3
Saenko Dmitry <dmitryssaenko@gmail.com>
2021-08-26 22:38:13 +0300

    fix collect key-value struct

--- tasks/collect_key_value_struct/main.go
+++ tasks/collect_key_value_struct/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"container/list"
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -10,14 +13,95 @@ type Tree struct {
 	inner map[string]*Tree
 }
 
+// recursive
+func (t *Tree) String() string {
+	if len(t.inner) == 0 {
+		return buildVertex("root", t.value)
+	}
 
-func recursiveCollect() {
+	var resultBuffer bytes.Buffer
+	buf := make([]bytes.Buffer, 0)
+	buf = append(buf, bytes.Buffer{})
 
+	buf[0].WriteString(buildVertex("root", t.value))
+	for key := range t.inner {
+		recursive(t.inner[key], key, 1, &buf)
+	}
+
+	for i := range buf {
+		resultBuffer.WriteString(buf[i].String())
+		resultBuffer.WriteString("\n")
+	}
+	return resultBuffer.String()
+}
+
+func recursive(vertex *Tree, key string, level int, buf *[]bytes.Buffer) {
+	if len(*buf) <= level {
+		*buf = append(*buf, bytes.Buffer{})
+	}
+	(*buf)[level].WriteString(buildVertex(key, vertex.value) + " ")
+	for key := range vertex.inner {
+		recursive(vertex.inner[key], key, level + 1, buf)
+	}
+}
+
+func (t *Tree) String1() string {
+	var buf bytes.Buffer
+
+	currQueue := list.New()
+	currQueue.PushBack(t)
+	if len(t.inner) == 0 {
+		return buildVertex("root", t.value)
+	}
+	currLevel := len(t.inner) - 1
+	currIndex := 0
+	for currQueue.Len() != 0 {
+		elem := currQueue.Front()
+		currValue := elem.Value.(*Tree).value
+		buf.WriteString(buildVertex("root", currValue))
+		if currIndex == currLevel {
+			buf.WriteString("\n")
+		}
+		items := elem.Value.(*Tree).inner
+		if currIndex == currLevel {
+
+		}
+		for key := range items {
+			buf.WriteString(key + " ")
+			currQueue.PushBack(items[key])
+		}
+		currQueue.Remove(elem)
+		currIndex++
+	}
+	return buf.String()
+}
+
+func buildVertex(key string, value int) string {
+	if value == 0 {
+		return key
+	}
+	return key + ":" + strconv.Itoa(value)
 }
 
 func setValueByPath(path []string, value int, root *Tree) {
-	// TODO : implement logic for setting value by path
+	if len(path) == 0 {
+		return
+	} else if len(path) == 1 {
+		if _, ok := root.inner[path[0]]; !ok {
+			root.inner[path[0]] = &Tree{
+				value: value,
+				inner: make(map[string]*Tree),
+			}
+		}
+	}
 
+	if _, ok := root.inner[path[0]]; !ok {
+		root.inner[path[0]] = &Tree{
+			inner: make(map[string]*Tree),
+		}
+	}
+	newRoot := root.inner[path[0]]
+	setValueByPath(path[1:], value, newRoot)
 }
 
 func build(input string, accum *Tree) {
@@ -27,19 +111,23 @@ func build(input string, accum *Tree) {
 }
 
 func buildAll(inputs []string) *Tree {
-	root := &Tree{value: nil, inner: make(map[string]*Tree)}
+	root := &Tree{inner: make(map[string]*Tree)}
 	for i := range inputs {
 		build(inputs[i], root)
 	}
 	return root
 }
 
+// [key1 [ key2 [] key3 [] ] key3]
+
 func main() {
 	inputs := []string{
-		"key1.key2=value1",
-		"key1.key2.key5=value2",
-		"key3.key4=value3",
-		"key3=value3",
+		"key1.key2=4",
+		"key1.key2.key5=2",
+		"key1.key3.key7=8",
+		"key10.key4=9",
+		"key11=10",
 	}
-	buildAll(inputs)
+	fmt.Println()
+	fmt.Println(buildAll(inputs))
 }
