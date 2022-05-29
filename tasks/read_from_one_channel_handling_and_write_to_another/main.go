41556319b0d8432c6c34d37eec831cec965de6fd
Dmitry Saenko <dmitrysaenko@Dmitrys-MacBook-Pro.local>
2022-05-29 23:01:46 +0300

    modificate read from one and write to another channel task

--- tasks/read_from_one_channel_handling_and_write_to_another/main.go
+++ tasks/read_from_one_channel_handling_and_write_to_another/main.go
@@ -1,26 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// merging N channels into one
+func handleChannels(in <-chan int, out chan<- int) {
+	for i := range in {
+		out <- i * i
+	}
+	close(out)
+	return
+}
 
 func main() {
 	naturals := make(chan int)
 	squares := make(chan int)
 
 	go func() {
-		for _, num := range []int{1,2,3} {
+		for _, num := range []int{1, 2, 3} {
 			naturals <- num
 		}
 		close(naturals)
 	}()
 
-	go func() {
-		for id := range naturals {
-			squares <- id * id
-		}
-		close(squares)
-	}()
+	go handleChannels(naturals, squares)
 
-	for id := range squares {
-		fmt.Println(id)
+	for i := range squares {
+		fmt.Println(i)
 	}
 }
